82b52f436eb1249378e3db8be8eeab0152f23213
nadiaMahfoud <nadiamahfoud48@gmail.com>
2023-06-26 12:00:05 -0700

    few modifications

--- list_control.c
+++ list_control.c
@@ -2,14 +2,14 @@
 
 
 /**
- * execute_builtin_function - searches for a match
- * and executes the associated builtin
- * @prog: struct for the program's data
+ * show_builtin_cmd - to look for a match
+ * and execute the associated builtin
+ * @prog: structure for the program's data
  * Return: Returns the return value of the executed
- * function if there is a match,
- * otherwise returns -1.
+ * function if there's a match,
+ * otherwise it returns -1.
  **/
-int execute_builtin_function(prog_data *prog)
+int show_builtin_cmd(prog_data *prog)
 {
 	int it;
 
@@ -19,18 +19,18 @@ int execute_builtin_function(prog_data *prog)
 		{"cd", change_dir},
 		{"alias_name", manage_alias},
 		{"env", exe_env_command},
-		{"setenv", set_environment},
+		{"setenv", init_environment},
 		{"unsetenv", unset_environment},
 		{NULL, NULL}
 	};
-	/* Walk through the structure */
+	/* Walk along the structure */
 	for (it = 0; opt[it].builtin != NULL; it++)
-	{/* If there is a match between the given command and a builtin */
+	{/* If there's a match between the given command and a builtin */
 		if (str_comp(opt[it].builtin, prog->cmd, 0))
-		{/* Execute the function and return its return value */
+		{/* To Execute the function and return its return value */
 			return (opt[it].function(prog));
 		}
 	}
-	/* If there is no match, return -1 */
+	/* If there's no match, return -1 */
 	return (-1);
 }
